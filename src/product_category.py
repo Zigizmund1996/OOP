3c6be6ea98aafe97ddc84a51690465526e5b4c92
Yan <gzigizmund@gmail.com>
2024-08-19 23:01:46 +0400

    add flake8

--- src/product_category.py
+++ src/product_category.py
@@ -1,10 +1,10 @@
 import json
 import logging
 import os
-from typing import Optional, List
+from typing import List, Optional
 
 project_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-log_file = 'product_category.log'
+log_file = "product_category.log"
 log_directory = os.path.join(project_root, "logs")
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
@@ -12,10 +12,9 @@ log_path = os.path.join(log_directory, log_file)
 
 # log in console&file
 logging.basicConfig(
-    filename=log_path,
-    filemode='w',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s')
+    filename=log_path, filemode="w", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
@@ -24,12 +23,19 @@ class Product:
         self.price = price
         self.quantity = quantity
         logging.info(f"Product created: {self}, {name}")
+
+
 class Category:
     total_categories: int = 0
     total_products: int = 0
 
-    def __init__(self, name: str, description: str, products: Optional[List[Product]] = None,
-                 products_file: Optional[str] = None):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        products: Optional[List[Product]] = None,
+        products_file: Optional[str] = None,
+    ):
         self.name = name
         self.description = description
         self.products: List[Product] = products if products is not None else []
@@ -44,7 +50,7 @@ class Category:
         # adding products from a JSON-file, if the file exists
         if products_file:
             try:
-                with open('products.json', 'r') as file:
+                with open("products.json", "r") as file:
                     products_list = json.load(file)
                     for product_data in products_list:
                         product = Product(**product_data)
@@ -53,4 +59,3 @@ class Category:
                         logging.info(f"Added product {product} to category {self.name}")
             except (json.JSONDecodeError, FileNotFoundError) as e:
                 logging.error(f"Error reading products file: {e}")
-
